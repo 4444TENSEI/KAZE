f8e721fc6ad70f8b16bc387215210437fce1ce29
夜風 <2479757568@qq.com>
2025-01-30 23:36:16 +0800

    -

--- src/stores/modules/user/profileUrl.ts
+++ src/stores/modules/user/profileUrl.ts
@@ -1,13 +1,10 @@
 import { defineStore } from 'pinia'
 import pb from '@/api/pocketbase'
-import { UserInfo } from '@/types/userInfo'
+import { type UserInfo } from '@/types/userInfo'
 import { PB_BASE_URL, DEFAULT_AVATAR_URL, DEFAULT_BACKGROUND_URL } from '@/config/url'
 
-/** 本地用户数据 */
-const userLocalData = pb.authStore.record
-
 const useUserInfoStore = defineStore('userInfo', () => {
-  // 用户信息
+  /** 用户信息 */
   const userInfo = ref<UserInfo>({
     id: '',
     nickname: '',
@@ -16,21 +13,20 @@ const useUserInfoStore = defineStore('userInfo', () => {
     backgroundUrl: DEFAULT_BACKGROUND_URL,
   })
 
-  /**
-   * 更新用户信息
-   */
+  /** 更新用户信息 */
   function updateUserInfo() {
-    if (!userLocalData) {
-      return console.error('用户数据为空')
+    const localProfile = pb.authStore.record
+    if (!localProfile) {
+      return console.error('用户本地数据为空')
     }
-    userInfo.value.id = userLocalData.id
-    userInfo.value.nickname = userLocalData.nickname
-    userInfo.value.email = userLocalData.email
-    if (userLocalData.avatar) {
-      userInfo.value.avatarUrl = `${PB_BASE_URL}/api/files/${userLocalData.collectionName}/${userLocalData.id}/${userLocalData.avatar}`
+    userInfo.value.id = localProfile.id
+    userInfo.value.nickname = localProfile.nickname
+    userInfo.value.email = localProfile.email
+    if (localProfile.avatar) {
+      userInfo.value.avatarUrl = `${PB_BASE_URL}/api/files/${localProfile.collectionName}/${localProfile.id}/${localProfile.avatar}`
     }
-    if (userLocalData.background) {
-      userInfo.value.backgroundUrl = `${PB_BASE_URL}/api/files/${userLocalData.collectionName}/${userLocalData.id}/${userLocalData.background}`
+    if (localProfile.background) {
+      userInfo.value.backgroundUrl = `${PB_BASE_URL}/api/files/${localProfile.collectionName}/${localProfile.id}/${localProfile.background}`
     }
     console.log('用户信息已更新', userInfo.value)
   }
