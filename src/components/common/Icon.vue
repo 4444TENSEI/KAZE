f320bdeb58fe4788023f3eab7e860c033061ff63
夜風 <2479757568@qq.com>
2025-02-02 21:16:08 +0800

    -

--- src/components/common/Icon.vue
+++ src/components/common/Icon.vue
@@ -5,7 +5,7 @@
 <script lang="ts" setup>
   import { Icon } from '@iconify/vue'
 
-  export const props = defineProps({
+  const props = defineProps({
     icon: {
       type: String,
       required: true,
