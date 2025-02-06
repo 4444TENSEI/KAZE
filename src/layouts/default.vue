e69fa0244b90e618d7d2f8ac8eecb78b7ed715ba
夜風 <2479757568@qq.com>
2025-02-07 01:46:31 +0800

    feat: 新增vercel数据监测

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
-  <router-view />
   <AppFrame v-if="pb.authStore.isValid" />
   <Footer v-else />
   <Toast />
+  <router-view />
+  <Analytics />
 </template>
 
 <script lang="ts" setup>
   import pb from '@/api/pocketbase'
+  import { Analytics } from '@vercel/analytics/vue'
 </script>
